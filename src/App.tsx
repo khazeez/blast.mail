eafa68f8d05517fd2e196efa0c84c710079e86cc
khazeez <cakirul211001@gmail.com>
2026-03-06 14:42:59 +0700

    fix: add React Router v7 future flags to suppress warnings

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <Routes>
               <Route path="/" element={<Landing />} />
               <Route path="/pricing" element={<Pricing />} />
